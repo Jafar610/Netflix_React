0311cf9aee4f544d9ae83d7df9875abec6049143
Jafar610 <jafarmohamed955@gmail.com>
2026-01-02 23:53:17 +0300

    file udpated

--- Netflix_Clone/src/components/Header.jsx
+++ Netflix_Clone/src/components/Header.jsx
@@ -1,11 +1,14 @@
 import logo from '../assets/images/logo.png'
+import 'header.css'
 
 function Header() {
   return (
     <>
+    <div className="header">
       <img src={logo} alt="Netflix Logo" />
 
       <button>Login</button>
+    </div>
     </>
   )
 }
